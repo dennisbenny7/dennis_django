ec7678507765b566204a710b4d5cfb13f4acdd12
dennisbenny7 <dennisbenny72@gmail.com>
2021-07-20 20:00:57 +0530

    initi

---
+++ m.py
@@ -0,0 +1,219 @@
+# p= int(input("enter p"))
+# r= int(input("enter r"))
+# n= int(input("enter n"))
+# s=(p*r*n)/100
+# print(s)
+######################
+# for x in range(0,5):
+#     print("iam a programmer")
+
+# def squre():
+#     for x in range(1,10):
+#         x=x*x
+#         print(x)
+# squre()        
+33333333333333333333333333333333333333333
+# cv2.imshow("Picture", image)
+# resize = cv2.resize(image, (256,256))
+# cv2.imshow("Picture", image)
+# cv2.imshow("Resized Picture", resize) # Resized Image
+# cv2.waitKey(0)
+#################################
+# l=["hai","avodha","how","are","you"]
+# print(l[2])
+# l.append("i am fine")
+# print(l)
+# l.insert(3, "tacos")
+# print(l)
+####################################
+
+# def bmi(height,weight):
+
+#     BMI = weight / (height/100)**2
+#     return BMI
+
+# height = float(input("Enter your height in cm: "))
+# weight = float(input("Enter your weight in kg: "))
+# BMI=bmi(height,weight)
+# print(f"You BMI is {BMI}")
+#######################################
+
+# f = open('demo.txt','r+')
+# f.write("hai avoda jj ")
+# print(f.read())
+
+# f.close()
+# a=open("demo.txt",'a+')
+
+# a.write("how are you")
+# print(a.read)
+# a.close()
+##########################################3
+
+
+# ##########################333333
+
+
+
+# for x in range(1,101):
+#     if x%2==0 :
+#         print(x, "is even number")
+#     elif x%2!=0:
+#         print(x,"is odd number")
+#############################################
+# x= lambda x:(x*(x+5))-2
+# print(x(5))
+######################################
+
+# class computer():
+#     def getspesi(self,a,b):
+#         self.a=a
+#         self.b=b
+#     def displayspesi(self):
+#         print(self.a)
+#         print(self.b)
+
+
+# class desktop(computer):
+#     def desktopsp(self):
+
+#         a="desktop have Lots of memory space \n easy to upgrade\n "
+#         b= "desktop have no battery"
+
+#         self.getspesi(a,b)
+
+
+
+# class laptop(computer):
+#     def laptopsp(self):
+
+#         a=" laptop internet access"
+#         b="laptop is using battery "
+#         self.getspesi(a,b)
+
+
+# a = input("enter desktop or laptop")
+# if a== "desktop":
+#     ob=desktop()
+#     ob.desktopsp()
+#     ob.displayspesi()
+# elif a== "laptop":
+#     ob=laptop()
+#     ob.laptopsp()
+#     ob.displayspesi()
+
+    
+
+
+
+    
+
+
+
+##################################################
+# a={"a":"b","c":"d","e":"f"
+# }
+# c=a.values()
+# d=list(c)
+
+
+
+
+# b=list(a)
+# print(d+b)
+
+# for i in d+b:
+    
+#     if i>=2:
+# class a:
+#     def __init__(self):
+        
+#         print("class aconstructor")
+# class b(a):
+#      def __init__(self):
+#         super().__init__()
+#         print("class  bconstructor")
+# class c(b):
+#      def __init__(self):
+#         super().__init__()
+#         print("class cconstructor")
+# o=c()
+
+
+# from numpy imp==ort*
+
+
+
+
+
+
+# b=a.view()
+# print(a,id(a))
+# print(b,id(b))
+
+# print(a)
+# 
+# a=10
+# a=[1,2,3]
+# b=a.copy()
+  # print(b)
+# from DENNIPYTHON import *
+
+# # a=denniinput(" enter your name")
+# # denniprint(a)
+# b=denni_list()
+# b=["aml punda","amal punda"]
+
+# denni_karakk(b)#loop and print method by dennni
+
+# amalpunda=denni_list(["punda amal","punda amal","klc hater"])
+# amalkoothi=denni_karakk_looponly(amalpunda)#dennis loop
+# print(amalkoothi)
+# ob=lit()
+# lit.mer(ob)
+# for i in range(1,2):
+
+
+#               print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(type(a))
+# print(id(a))
+# print(id(a))
+# print(type(a))
+class a:
+
+  acc=10
+
+
+
+
+
+
+a.acc=111
+
+ob=a()
+print(ob.acc)
